0b899b937cea2a0b3be8b30e2db3b3564a0c9e7b
YimyAsprilla <yimi.mosquera@est.iudigital.edu.co>
2025-10-05 12:05:11 -0500

    Se crea la clase viejes con sus respectivos atributos. Se crea el metodo constructor con sus respectivos getters Se crea el metodo agregrar y eliminar clentes. Se crea el metodo calcular costo.

--- src/main/java/modelo/Viajes.java
+++ src/main/java/modelo/Viajes.java
@@ -1,4 +1,53 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Viajes {
+
+    private String id;
+    private String origen;
+    private String destino;
+    private Vehiculo vehiculo; // Relación de composición con Vehiculo
+    private Conductor conductor; // Relación de composición con Conductor
+    private List<Cliente> clientes; // Relación de agregación con Cliente
+    private double tarifaBase;
+
+    public Viajes(String id, String origen, String destino, Vehiculo vehiculo, Conductor conductor, double tarifaBase) {
+        this.id = id;
+        this.origen = origen;
+        this.destino = destino;
+        this.vehiculo = vehiculo;
+        this.conductor = conductor;
+        this.clientes = new ArrayList<>();
+        this.tarifaBase = tarifaBase;
+    }
+
+    // Métodos para agregar y eliminar clientes
+    public boolean agregarCliente(Cliente cliente) {
+        if (clientes.size() < vehiculo.getCapacidad()) {
+            clientes.add(cliente);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double calcularCostoPorCliente(){
+        int ocupados = Math.max(1, clientes.size()); // Evitar división por cero
+        return tarifaBase / ocupados;
+    }
+
+    // Getters
+
+    public String getId() {return id;}
+    public List<Cliente> getClientes() {return clientes; }
+    public Vehiculo getVehiculo() {return vehiculo; }
+    public Conductor getConductor() {return conductor; }
+
+    @Override
+    public String toString() {
+        return String.format("Viaje ID: %s, Origen: %s, Destino: %s, Vehículo: [%s], Conductor: [%s], Clientes: %d, Tarifa Base: %.2f",
+                id, origen, destino, vehiculo, conductor, clientes.size(), tarifaBase);
+    }
 }
