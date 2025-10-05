3681af62733c52c899be0dab5face3c293d51991
marlon <marlon.monterrosa.m@gmail.com>
2025-10-05 12:21:19 -0500

    comentarios

--- src/main/java/patron_diseno/SistemaTransporte.java
+++ src/main/java/patron_diseno/SistemaTransporte.java
@@ -4,23 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 import modelo.Viajes;
 
-public class SistemaTransporte {
-    private static SistemaTransporte instancia;
-    private List<Viajes> viajes = new ArrayList<>();
+//Singleton que administra la colección de viajes del sistema.
 
+public class SistemaTransporte {
+    private static SistemaTransporte instancia; // instancia única
+    private List<Viajes> viajes = new ArrayList<>(); // almacenamiento en memoria
+
+    // Constructor privado para forzar patrón Singleton
     private SistemaTransporte() {}
 
+    // Devuelve la instancia única,  si es necesario
     public static SistemaTransporte getInstancia() {
         if (instancia == null) {
             instancia = new SistemaTransporte();
         }
         return instancia;
     }
-
+    // Agrega un viaje a la lista
     public void agregarViaje(Viajes v) {
         viajes.add(v);
     }
-
+    // Retorna la lista de viajes
     public List<Viajes> getViajes() {
         return viajes;
     }
